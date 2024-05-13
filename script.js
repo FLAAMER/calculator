f08f6492345af4dbaf8c13b5e1e78cb38cee6876
flaamer <88228748+FLAAMER@users.noreply.github.com>
2024-05-13 07:51:48 +0530

    Create operate function
     - Create a new function operate that takes an operator and 2 numbers
       and then calls one of the above functions on the numbers.

--- script.js
+++ script.js
@@ -17,3 +17,15 @@ function divide(num1, num2) {
 let num1
 let num2
 let operator
+
+function operate(num1, num2, operator) {
+    if (operator === "+") {
+        add(num1, num2)
+    } else if (operator === "-") {
+        subtract(num1, num2)
+    } else if (operator === "*") {
+        multiply(num1, num2)
+    } else if (operator === "/") {
+        divide(num1, num2)
+    }
+}
