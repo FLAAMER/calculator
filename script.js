e577a8bdc07d95d7ba0ebb75a18a7978b4a5af92
flaamer <88228748+FLAAMER@users.noreply.github.com>
2024-05-16 17:54:18 +0530

    Populate display when number buttons are clicked

--- script.js
+++ script.js
@@ -29,3 +29,23 @@ function operate(num1, operator, num2) {
         
     }
 }
+
+let calcNumberButtons = document.querySelectorAll("div.calc-button.number")
+calcNumberButtons = Array.from(calcNumberButtons)
+
+calcNumberButtons.forEach((button) => button.addEventListener("click", () => {
+    if (document.querySelector("div#display-number").textContent === "0") {
+        document.querySelector("div#display-number").textContent = button.textContent
+    } else {
+        document.querySelector("div#display-number").textContent += button.textContent
+    }
+}))
+
+let displayValue
+console.log(displayValue)
+let calcButtons = document.querySelectorAll("div.calc-button")
+
+calcButtons.forEach((button) => button.addEventListener("click", () => displayValue = document.querySelector("div#display-number").textContent))
+
+let decimal = document.querySelector("div#decimal")
+decimal.addEventListener("click", () => {if (!Array.from(displayValue).includes(".")) document.querySelector("div#display-number").textContent += "."})
